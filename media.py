8f3e7835e4642459deb1413d15e0fd43b1fe49e5
Matheus Leuck <matheusrodrigues91@gmail.com>
2018-08-18 19:54:19 -0300

    Arquivo modificado para calcular o maior valor dentre 3 valores.

--- media.py
+++ media.py
@@ -1,7 +1,19 @@
-﻿# Calculador de média
-
-from random import randint
+﻿from random import randint
 valor1 = randint(0,100)
 valor2 = randint(0,100)
-media = valor1 + valor2 / 2
-print (media)
+valor3 = randint(0,100)
+media = valor1 + valor2 / 2 #Calculo de média
+print ("Primeiro valor:" , valor1)
+print ("Segundo valor:" , valor2)
+print ("Terceiro valor:" , valor3)
+print ("Media do primeiro e segundo valor:" , media)
+print ("Calculo de maior valor(primeiro, segundo, terceiro):")
+#Calculo de maior valor
+if valor1 > valor2 and valor1 > valor3:
+	print ("Maior valor: Primeiro ->", valor1)
+elif valor2 > valor1 and valor2 > valor3:
+	print ("Maior valor: Segundo ->", valor2)
+elif valor3 > valor1 and valor3 > valor2:
+	print ("Maior valor: Terceiro ->", valor3)
+else:
+	print ("Existem valores repetidos como maior valor.")
